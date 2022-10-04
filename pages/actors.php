24a33a7052ded1a98942c53ad78bdb0d3099b4cf
Paulo Ricardo Krachliwski <prkrachliwski10@gmail.com>
2022-10-03 22:21:54 -0300

    updates

--- pages/actors.php
+++ pages/actors.php
@@ -11,14 +11,29 @@
     </div>
 
     <?php
-    $arquivo = URL."characters";
+    $arquivo = URL . "characters";
 
     $dados = file_get_contents($arquivo);
 
     $dados = json_decode($dados);
 
     foreach ($dados as $actors) {
-        echo "<p>{$actors->name}</p>";
+        $poster = $actors->img;
+
+    ?>
+
+        <div class='card'>
+            <div class='row'>
+                <div class="col-12 cold-md-9">
+                    <h1 class='actors-name'><?= $actors->name ?></h1>
+                </div>
+
+                <div class="col-12 col-md-3">
+                    <img src="<?= $poster ?>" alt="<?= $actors->name ?>" class='card-image'>
+                </div>
+            </div>
+        </div>
+    <?php
     }
     ?>
 </body>
